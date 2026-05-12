574e1a12705b7477c1d0e6834cb26aca5177d7db
Yazel Hipolito <yazelhipolito@gmail.com>
2026-05-12 05:45:45 -0600

    create setup.sql

---
+++ src/setup.sql
@@ -0,0 +1,10 @@
+-- ========================================
+-- Organization Table
+-- ========================================
+CREATE TABLE organization (
+    organization_id SERIAL PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    description TEXT NOT NULL,
+    contact_email VARCHAR(255) NOT NULL,
+    logo_filename VARCHAR(255) NOT NULL
+);
